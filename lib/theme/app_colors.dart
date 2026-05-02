980ca15e7caa8986d0b5b7de3913f9fed925a5d7
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2026-05-02 16:22:34 +0200

    [STYLE]Add app theme color constants

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const secondaryTextOnPrimary = Colors.white70;
+  static const mainBackgroundColor = Color.fromARGB(255, 240, 241, 243);
+  
+  static const secondaryTextOnPrimary = Color.fromARGB(179, 255, 255, 255);
+
+  static const highlightedItemBackground = Color.fromRGBO(225, 235, 248, 1);
+  static const highlightedItemText = Color.fromRGBO(37, 132, 248, 1);
+  static const highlightedItemBorder = Color.fromRGBO(37, 132, 248, 0.4);
+
+  static const forecastButtonBackground = Color.fromRGBO(230, 233, 235, 1);
+  static const forecastButtonText = Color.fromRGBO(84, 96, 113, 1);
 }
