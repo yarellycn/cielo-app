d4e2b29d9d546d37c84519f72df827ad7b496752
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2026-05-03 12:26:01 +0200

    [STYLE]Update app bar with logo and icon

--- lib/widgets/cielo_app_bar.dart
+++ lib/widgets/cielo_app_bar.dart
@@ -1,24 +1,35 @@
+import 'package:cielo_app/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CieloAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CieloAppBar({super.key});
+  static const appBarHeight = 75.00;
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(appBarHeight);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: const Icon(Icons.cloud),
-      backgroundColor: const Color.fromARGB(255, 15, 130, 207),
+      toolbarHeight: appBarHeight,
+      leadingWidth: 60,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 20.00),
+        child: Image.asset('assets/icons/app_icon.png', filterQuality: FilterQuality.medium,),
+      ),
+      backgroundColor: AppColors.mainBackgroundColor,
+      shape: const Border(bottom: BorderSide(color: AppColors.forecastButtonBackground)),
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Cielo', overflow: TextOverflow.ellipsis),
+          Image.asset('assets/logos/app_logo.png', scale: 8.00, filterQuality: FilterQuality.medium,),
           Text(
             'HISTORIQUE ET PRÉVISIONS MÉTÉO',
-            style: Theme.of(context).textTheme.bodyMedium,
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(
+              color: AppColors.logoColor,
+              fontWeight: FontWeight.bold,
+            ),
             overflow: TextOverflow.ellipsis,
           ),
         ],
