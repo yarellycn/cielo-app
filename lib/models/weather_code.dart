1c7cfb890b80fe8744eb561185f193eca524c269
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2026-05-03 22:32:44 +0200

    [REFACTOR]Change weather code descriptions to french

--- lib/models/weather_code.dart
+++ lib/models/weather_code.dart
@@ -1,32 +1,44 @@
 enum WeatherCode {
-  clearSky(0, 'Clear sky'),
-  mainlyClear(1, 'Mainly clear'),
-  partlyCloudy(2, 'Partly cloudy'),
-  overcast(3, 'Overcast'),
-  fog(45, 'Fog'),
-  rimeFog(48, 'Depositing rime fog'),
-  lightDrizzle(51, 'Light drizzle'),
-  moderateDrizzle(53, 'Moderate drizzle'),
-  denseDrizzle(55, 'Dense drizzle'),
-  lightFreezingDrizzle(56, 'Light freezing drizzle'),
-  denseFreezingDrizzle(57, 'Dense freezing drizzle'),
-  slightRain(61, 'Slight rain'),
-  moderateRain(63, 'Moderate rain'),
-  heavyRain(65, 'Heavy rain'),
-  lightFreezingRain(66, 'Light freezing rain'),
-  heavyFreezingRain(67, 'Heavy freezing rain'),
-  slightSnowFall(71, 'Slight snow fall'),
-  moderateSnowFall(73, 'Moderate snow fall'),
-  heavySnowFall(75, 'Heavy snow fall'),
-  snowGrains(77, 'Snow grains'),
-  slightRainShowers(80, 'Slight rain showers'),
-  moderateRainShowers(81, 'Moderate rain showers'),
-  violentRainShowers(82, 'Violent rain showers'),
-  slightSnowShowers(85, 'Slight snow showers'),
-  heavySnowShowers(86, 'Heavy snow showers'),
-  slightThunderstorm(95, 'Slight Thunderstorm'),
-  thunderstormSlightHail(96, 'Thunderstorm with slight hail'),
-  thunderstormHeavyHail(99, 'Thunderstorm with heavy hail');
+  clearSky(0, 'Ciel dégagé'), // Clear sky
+  mainlyClear(1, 'Principalement dégagé'), // Mainly clear
+  partlyCloudy(2, 'Partiellement nuageux'), // Partly cloudy
+  overcast(3, 'Couvert'), // Overcast
+  fog(45, 'Brouillard'), // Fog
+  rimeFog(48, 'Brouillard givrant'), // Depositing rime fog
+  lightDrizzle(51, 'Bruine légère'), // Light drizzle
+  moderateDrizzle(53, 'Bruine modérée'), // Moderate drizzle
+  denseDrizzle(55, 'Bruine dense'), // Dense drizzle
+  lightFreezingDrizzle(
+    56,
+    'Bruine verglaçante légère',
+  ), // Light freezing drizzle
+  denseFreezingDrizzle(
+    57,
+    'Bruine verglaçante dense',
+  ), // Dense freezing drizzle
+  slightRain(61, 'Pluie faible'), // Slight rain
+  moderateRain(63, 'Pluie modérée'), // Moderate rain
+  heavyRain(65, 'Forte pluie'), // Heavy rain
+  lightFreezingRain(66, 'Pluie verglaçante légère'), // Light freezing rain
+  heavyFreezingRain(67, 'Forte pluie verglaçante'), // Heavy freezing rain
+  slightSnowFall(71, 'Faibles chutes de neige'), // Slight snow fall
+  moderateSnowFall(73, 'Chutes de neige modérées'), // Moderate snow fall
+  heavySnowFall(75, 'Fortes chutes de neige'), // Heavy snow fall
+  snowGrains(77, 'Neige en grains'), // Snow grains
+  slightRainShowers(80, 'Averses de pluie faibles'), // Slight rain showers
+  moderateRainShowers(81, 'Averses de pluie modérées'), // Moderate rain showers
+  violentRainShowers(82, 'Averses de pluie violentes'), // Violent rain showers
+  slightSnowShowers(85, 'Averses de neige faibles'), // Slight snow showers
+  heavySnowShowers(86, 'Fortes averses de neige'), // Heavy snow showers
+  slightThunderstorm(95, 'Orage faible'), // Slight Thunderstorm
+  thunderstormSlightHail(
+    96,
+    'Orage avec grêle faible',
+  ), // Thunderstorm with slight hail
+  thunderstormHeavyHail(
+    99,
+    'Orage avec forte grêle',
+  ); // Thunderstorm with heavy hail
 
   const WeatherCode(this.code, this.description);
   final int code;
